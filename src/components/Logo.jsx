5bc0762f69596e492c2b6a7ad438dd257803872c
omarcamir <omarcamirr@gmail.com>
2025-08-27 03:17:04 +0300

    feat(Gemini): integrate with Gemini

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div className="flex items-center justify-center gap-5">
-      <img src="/AiChaiBot.webp" alt="logo" className="w-16" />
+      <img src="/svg/AiChaiBot.webp" alt="logo" className="w-16" />
       <h1 className="text-3xl font-bold uppercase">
         Omar
         <span className="text-main-color">Chai-Bot</span>
